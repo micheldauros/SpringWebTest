a912daf0474bc2582345afc408f909780fb07d3a
ftoorto <m.f.d.y.n.a@live.com>
2022-05-22 02:57:10 +0800

    sql2

--- SpringMVCTest01/src/main/resources/1.sql
+++ SpringMVCTest01/src/main/resources/1.sql
@@ -1,3 +1,31 @@
+SELECT
+    CASE occupation
+        WHEN 1 THEN
+            '学⽣'
+        WHEN 2 THEN
+            '教师'
+        WHEN 3 THEN
+            '医⽣'
+        WHEN 4 THEN
+            '护⼠'
+        WHEN 5 THEN
+            '程序员'
+        ELSE
+            '未知'
+        END AS `职业`,
+    CAST(
+            CONCAT(
+                    '男:',
+                    COUNT(IF(sex = 1, 1, NULL)),
+                    ', ⼥:',
+                    COUNT(IF(sex = 2, 1, NULL))
+                ) AS CHAR
+        ) AS `性别`
+FROM
+    `person01`
+GROUP BY
+    occupation;
+
 use sakila;
 select c1.country country, c2.city city from country c1 left join  city c2 on c1.country_id = c2.country_id;
 delete
