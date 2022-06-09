60babc14c4e274c24f12b8052076d39da3d0e1b5
ftoorto <m.f.d.y.n.a@live.com>
2022-06-09 20:54:46 +0800

    springmvc basic 乱码问题 resolved

--- SpringMVCTest01/src/main/java/com/yy/controller/Controllers.java
+++ SpringMVCTest01/src/main/java/com/yy/controller/Controllers.java
@@ -1,6 +1,7 @@
 package com.yy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
