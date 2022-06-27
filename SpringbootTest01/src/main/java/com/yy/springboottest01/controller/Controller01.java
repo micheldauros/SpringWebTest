e908cab7fdcc830f0b68bba2b5fa6c96eada0ba1
ftoorto <m.f.d.y.n.a@live.com>
2022-06-27 19:20:12 +0800

    springboot9

--- SpringbootTest01/src/main/java/com/yy/springboottest01/controller/Controller01.java
+++ SpringbootTest01/src/main/java/com/yy/springboottest01/controller/Controller01.java
@@ -1,13 +1,14 @@
 package com.yy.springboottest01.controller;
 
 import com.yy.springboottest01.pojo.Actor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller01 {
     @RequestMapping("/controller01")
-    public String controller01(Actor actor){
+    public String controller01(@Validated Actor actor){
         System.out.println(actor);
         return actor.getFirstName()+actor.getLastName();
     }
--- SpringbootTest01/src/main/java/com/yy/springboottest01/pojo/Actor.java
+++ SpringbootTest01/src/main/java/com/yy/springboottest01/pojo/Actor.java
@@ -3,6 +3,7 @@ package com.yy.springboottest01.pojo;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -10,7 +11,8 @@ public class Actor {
     private Integer actor_id;
     private String firstName;
     private String lastName;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @NonNull()
+    @DateTimeFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
+    @NotNull(message = "lastUpdate是空")
+
     private LocalDateTime lastUpdate;
 }
