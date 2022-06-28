6112f83cd909bc99cf64ea327b88b8a51da68330
ftoorto <m.f.d.y.n.a@live.com>
2022-06-28 19:53:21 +0800

    springboot11

--- SpringbootTest01/src/main/java/com/yy/springboottest01/SpringbootTest01Application.java
+++ SpringbootTest01/src/main/java/com/yy/springboottest01/SpringbootTest01Application.java
@@ -15,49 +15,51 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class SpringbootTest01Application {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootTest01Application.class);
-
-    @Bean
-    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
-                                            MessageListenerAdapter listenerAdapter) {
-
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
-
-        return container;
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTest01Application.class,args);
     }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
-    }
-
-    @Bean
-    Receiver receiver() {
-        return new Receiver();
-    }
-
-    @Bean
-    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
-        return new StringRedisTemplate(connectionFactory);
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        ApplicationContext ctx = SpringApplication.run(SpringbootTest01Application.class, args);
-
-        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
-        Receiver receiver = ctx.getBean(Receiver.class);
-
-        while (receiver.getCount() == 0) {
-
-            LOGGER.info("Sending message...");
-            template.convertAndSend("chat", "Hello from Redis!");
-            Thread.sleep(500L);
-        }
-
-        System.exit(0);
-    }
+//    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootTest01Application.class);
+//
+//    @Bean
+//    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+//                                            MessageListenerAdapter listenerAdapter) {
+//
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory);
+//        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+//
+//        return container;
+//    }
+//
+//    @Bean
+//    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+//        return new MessageListenerAdapter(receiver, "receiveMessage");
+//    }
+//
+//    @Bean
+//    Receiver receiver() {
+//        return new Receiver();
+//    }
+//
+//    @Bean
+//    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+//        return new StringRedisTemplate(connectionFactory);
+//    }
+//
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        ApplicationContext ctx = SpringApplication.run(SpringbootTest01Application.class, args);
+//
+//        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+//        Receiver receiver = ctx.getBean(Receiver.class);
+//
+//        while (receiver.getCount() == 0) {
+//
+//            LOGGER.info("Sending message...");
+//            template.convertAndSend("chat", "Hello from Redis!");
+//            Thread.sleep(500L);
+//        }
+//
+//        System.exit(0);
+//    }
 }
