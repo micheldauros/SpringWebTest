ba315e6752d78ed4a00a6b00a9aa831ff4fdf54e
ftoorto <m.f.d.y.n.a@live.com>
2022-07-20 20:55:13 +0800

    springboot13

--- SpringbootTest01/src/main/java/com/yy/springboottest01/SpringbootTest01Application.java
+++ SpringbootTest01/src/main/java/com/yy/springboottest01/SpringbootTest01Application.java
@@ -22,51 +22,45 @@ import java.util.List;
 import java.util.stream.Collectors;
 import com.mysql.cj.jdbc.Driver;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
-public class SpringbootTest01Application {
-
-    private static DataService dataService;
-
-    @Autowired
-    public void setDataService(DataService dataService){
-        SpringbootTest01Application.dataService=dataService;
-    }
-
-
+public class SpringbootTest01Application implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(SpringbootTest01Application.class);
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootTest01Application.class, args);
-        SpringbootTest01Application.dataService.JdbcService01();
+    public static void main(String args[]) {
+        SpringApplication.run(SpringbootTest01Application.class, args);
     }
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-//    @Override
-//    public void run(String... args) throws Exception {
-//        log.info("Creating tables");
-//
-//        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-//        jdbcTemplate.execute("CREATE TABLE customers(" +
-//                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
-//
-//        // Split up the array of whole names into an array of first/last names
-//        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-//                .map(name -> name.split(" "))
-//                .collect(Collectors.toList());
-//
-//        // Use a Java 8 stream to print out each tuple of the list
-//        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-//
-//        // Uses JdbcTemplate's batchUpdate operation to bulk load data
-//        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
-//
-//        log.info("Querying for customer records where first_name = 'Josh':");
-//        jdbcTemplate.query(
-//                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
-//                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-//        ).forEach(customer -> log.info(customer.toString()));
-//    }
+    @Override
+    public void run(String... strings) throws Exception {
+
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE  IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> log.info(customer.toString()));
+    }
 }
