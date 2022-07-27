677fbb00332fc78c1ff4527826681691b9f47e09
ftoorto <m.f.d.y.n.a@live.com>
2022-07-28 02:36:26 +0800

    mysql复杂语句

--- SpringMVCTest01/src/main/resources/1.sql
+++ SpringMVCTest01/src/main/resources/1.sql
@@ -59,3 +59,44 @@ select group_concat(sakila.film_actor.actor_id) actorList,sakila.film_actor.film
 #代替上面的查询，两张表都只遍历一次
 use sakila;
 select sakila.actor.actor_id,sakila.actor.last_name,sakila.actor.first_name,sakila.actor.last_update,sakila.film_actor.film_id from sakila.film_actor inner join sakila.actor  on film_actor.actor_id = actor.actor_id order by sakila.film_actor.film_id;
+
+
+
+create table split_test(
+                           id int auto_increment primary key,
+                           translation varchar(20) not null ,
+                           following varchar(20) not null
+);
+insert into split_test(translation, following) values
+                                                   ('apple','2,3'),
+                                                   ('banana','3,1'),
+                                                   ('pear','1,2');
+use mybatis_test;
+
+
+select
+    id,
+    translation,
+    (select translation from split_test where id in (select substring_index(following,',',1) from split_test)) first_follow,
+    (select translation from split_test where id in (select substring_index(following,',',-1) from split_test)) second_follow
+from split_test;
+
+select translation from split_test where id in (select substring_index(following,',',1) from split_test);
+select translation from split_test where id in (select substring_index(following,',',-1) from split_test);
+
+set global log_bin_trust_function_creators=TRUE;
+create function func1
+(tid int)
+    returns  varchar(20)
+    return
+        (select translation from split_test where id=tid);
+select
+    id,
+    translation,
+    func1(substring_index(following,',',1)) first_follow,
+    func1(substring_index(following,',',-1)) second_follow
+from split_test;
+
+select
+    id,
+    (select @ids:=GROUP_CONCAT(following) from split_test where find_in_set(id,following))
