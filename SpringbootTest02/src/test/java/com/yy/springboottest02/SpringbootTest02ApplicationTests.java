64c13f451b98c77bff34bf38c51cea37bbe4ec9a
ftoorto <m.f.d.y.n.a@live.com>
2022-08-03 18:24:57 +0800

    redis 2

--- SpringbootTest02/src/test/java/com/yy/springboottest02/SpringbootTest02ApplicationTests.java
+++ SpringbootTest02/src/test/java/com/yy/springboottest02/SpringbootTest02ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.yy.springboottest02;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class SpringbootTest02ApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
 
     @Test
     void contextLoads() {
+        redisTemplate.opsForValue().set("aaa","耶路撒冷jelusalum");
+        System.out.println(redisTemplate.opsForValue().get("aaa"));
     }
 
 }
