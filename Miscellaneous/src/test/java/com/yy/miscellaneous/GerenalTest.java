82f9c93d8c6582a8b5ef6da75a030492e2775858
ftoorto <m.f.d.y.n.a@live.com>
2022-08-23 11:17:45 +0800

    md5加密 md5Digest每次结果都不一样，为什么？

---
+++ Miscellaneous/src/test/java/com/yy/miscellaneous/GerenalTest.java
@@ -0,0 +1,26 @@
+package com.yy.miscellaneous;
+
+import com.google.common.base.Stopwatch;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.DigestUtils;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class GerenalTest {
+    @Test
+    public void stopWatch01() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+//        Thread.sleep(3000);
+        stopwatch.stop();
+        System.out.println(stopwatch.elapsed(TimeUnit.MICROSECONDS));
+    }
+    @Test
+    public void digtest01(){
+        for (int i = 0; i < 10; i++) {
+            byte[] b = DigestUtils.md5Digest("hello word".getBytes());
+            System.out.println(b);//每次结果都不一样，为什么？
+            System.out.println(Arrays.toString(b));
+        }
+    }
+}
