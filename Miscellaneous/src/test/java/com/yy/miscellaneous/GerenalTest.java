83311cf79dd206d28684586c23e093e97400479a
ftoorto <m.f.d.y.n.a@live.com>
2022-08-23 11:22:48 +0800

    md5加密 md5Digest每次结果都不一样，为什么？

--- Miscellaneous/src/test/java/com/yy/miscellaneous/GerenalTest.java
+++ Miscellaneous/src/test/java/com/yy/miscellaneous/GerenalTest.java
@@ -19,8 +19,12 @@ public class GerenalTest {
     public void digtest01(){
         for (int i = 0; i < 10; i++) {
             byte[] b = DigestUtils.md5Digest("hello word".getBytes());
+            byte[] d = DigestUtils.md5Digest("hello word".getBytes());
+
+            String c=Arrays.toString(b);
             System.out.println(b);//每次结果都不一样，为什么？
-            System.out.println(Arrays.toString(b));
+            System.out.println(c);
+            System.out.println(d);
         }
     }
 }
